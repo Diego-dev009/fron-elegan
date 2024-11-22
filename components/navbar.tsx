d91ad0b473d111b9ff6145e5a0613d7f38969c46
Diego-dev009 <diegomamaniquispe009@gmail.com>
2024-11-21 23:08:16 -0400

    creacionde navbar y menu de lis

---
+++ components/navbar.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { Heart, ShoppingCart, User } from "lucide-react";
+import { useRouter } from "next/navigation";
+import MenuList from "./menu-li";
+
+const Navbar = () =>{
+    const router = useRouter()
+    return(
+        
+        <div className="flex  justify-between p-4 mx-auto cursor-pointer sm:max-w-8xl md:max-w-6x1">
+            <h1 className="text-3xl" onClick={() =>router.push("/")} >Elegan
+                <samp className="font-bold">
+                    Store
+                </samp>
+            </h1>
+            <div className="items-center justify-between hiddeb sm:flex">
+                <MenuList/>
+            </div>
+            <div className="flex sm:hidden">
+                <p>intems menu mobile</p>
+            </div>
+            <div className="flex intems-center justify-between gap-2 sm:gap-7">
+                <ShoppingCart strokeWidth="1" className="cursor-pointer" onClick={() => router.push("/cart")} />
+                <Heart className="cursor-pointer" onClick={() => router.push("/lover-products")}  />
+
+                <User strokeWidth={1} className=" cursor-pointer" />
+                
+
+            </div>
+        </div>
+
+    );
+}
+
+export default Navbar;
